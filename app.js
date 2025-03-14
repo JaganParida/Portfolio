32ed754dca391126d092b1a0f3c186b1dc272d4a
JaganParida <jaganparida39064@gmail.com>
2025-03-14 20:32:12 +0530

    Poerfolio website

--- app.js
+++ app.js
@@ -17,3 +17,27 @@ let navlist = document.querySelector(".navlist");
 menuicon.addEventListener("click", () => {
   navlist.classList.toggle("hide");
 });
+
+/*Preview*/
+let previewContainer = document.querySelector(".projects-preview");
+let previewBox = previewContainer.querySelectorAll(".preview");
+
+document.querySelectorAll("#project-list .projects").forEach((projects) => {
+  projects.onclick = () => {
+    previewContainer.style.display = "flex";
+    let name = projects.getAttribute("data-name");
+    previewBox.forEach((preview) => {
+      let target = preview.getAttribute("data-target");
+      if (name == target) {
+        preview.classList.add("active");
+      }
+    });
+  };
+});
+
+previewBox.forEach((close) => {
+  close.querySelector(".fa-xmark").onclick = () => {
+    close.classList.remove("active");
+    previewContainer.style.display = "none";
+  };
+});
